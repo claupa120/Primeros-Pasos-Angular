21dbb11009e1e9f824f99552695cb11028f2e297
Claupa Paola Suarez Suarez <claupa1051@gmail.com>
2022-10-23 00:52:07 -0500

    cree los componentes agregar,editar, listar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AgregarEmpleadoComponent } from './components/agregar-empleado/agregar-empleado.component';
+import { EditarEmpleadoComponent } from './components/editar-empleado/editar-empleado.component';
+import { ListarEmpleadoComponent } from './components/listar-empleado/listar-empleado.component';
+const routes: Routes = [
+{path:'',pathMatch: 'full',redirectTo: 'agregar-empleado'},//ruta por default
+{path:'agregar-empleado',component: AgregarEmpleadoComponent},
+{path:'editar-empleado',component: EditarEmpleadoComponent},
+{path:'listar-empleado',component: ListarEmpleadoComponent},
 
-const routes: Routes = [];
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,20 @@ import { AppComponent } from './app.component';
 import { Hijo1Component } from './components/hijo1/hijo1.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatAutocompleteModule} from '@angular/material/autocomplete';
+import { AgregarEmpleadoComponent } from './components/agregar-empleado/agregar-empleado.component';
+import { EditarEmpleadoComponent } from './components/editar-empleado/editar-empleado.component';
+import { ListarEmpleadoComponent } from './components/listar-empleado/listar-empleado.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    Hijo1Component
+    Hijo1Component,
+    AgregarEmpleadoComponent,
+    EditarEmpleadoComponent,
+    ListarEmpleadoComponent,
+
+    
   ],
   imports: [
     BrowserModule,
